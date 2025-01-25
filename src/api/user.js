63ca31fde0d85e850f6cfbdf37ce7c3304850346
Emanuelly Wolski <wolski.reserva@gmail.com>
2025-01-24 21:56:02 -0300

    feat: :sparkles: Correção de commit. Crud de usuários finalizado.

--- src/api/user.js
+++ src/api/user.js
@@ -1,30 +1,26 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import baseQueryApi from './base-query';
-
 // Definição dos endpoints relacionados a usuários
 export const userApi = createApi({
   reducerPath: 'userApi',
   tagTypes: ['Users'],
   baseQuery: baseQueryApi,
   endpoints: (build) => ({
-
     // Endpoint para obter informações de um usuário
     getUser: build.query({
       query: (userId) => `/users/${userId}`,
     }),
-
     // Endpoint para listar todos os usuários
     getUsers: build.query({
-      query: () => '/user/all',
+      query: () => '/users',
     }),
 
     getStudents: build.query({
-      query: () => '/users/students',
+      query: () => '/auth/users/students',
       providesTags: ['Users'],
     }),
   }),
 });
-
 // Exportando os hooks gerados automaticamente pelo react toolkit query
 export const {
   useRegisterUserMutation,
